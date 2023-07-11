c0736804d4e44fcd98a6c33ba64611fac1ea0148
Artur Martynovych <arturmartynovych1@yahoo.com>
2023-07-11 17:40:08 +0200

    UI design and added testing notes

--- lib/Widgets/notes.dart
+++ lib/Widgets/notes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/Widgets/notes_list.dart';
 import 'package:todoapp/models/note.dart';
 
 class Notes extends StatefulWidget {
@@ -9,23 +10,56 @@ class Notes extends StatefulWidget {
 }
 
 class _NotesState extends State<Notes> {
-  final List<Note> _enteredNote = [];
+  final List<Note> _enteredNote = [
+    Note(
+      title: 'University',
+      note:
+          'Presentation for diploma, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est.',
+    ),
+    Note(
+      title: 'Work',
+      note: 'Go to work',
+    ),
+    Note(
+      title: 'Games',
+      note: 'Buy that super game',
+    ),
+    Note(
+      title: 'Home',
+      note: 'Prepare dinner',
+    ),
+  ];
+
+  void _removeNote(Note note) {
+    setState(() {
+      _enteredNote.remove(note);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    Widget mainContent = const Center(
+    Widget mainContent = Center(
       child: Text(
         "Brak notatek",
-        style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        ),
+        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+              fontSize: 36,
+            ),
       ),
     );
 
+    if (_enteredNote.isNotEmpty) {
+      mainContent = NotesList(
+        notes: _enteredNote,
+        onRemoveNote: _removeNote,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("ToDo"),
+        title: Text(
+          "ToDo",
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
       ),
       body: Column(
         children: [
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/Utils/color_utils.dart';
 import 'package:todoapp/Widgets/notes.dart';
 
 void main(List<String> args) {
@@ -6,6 +7,33 @@ void main(List<String> args) {
     MaterialApp(
       theme: ThemeData().copyWith(
         useMaterial3: true,
+        appBarTheme: AppBarTheme(
+          backgroundColor: hexStringToColor('#E1D4C8'),
+        ),
+        scaffoldBackgroundColor: hexStringToColor('#E1D4C8'),
+        cardColor: hexStringToColor('#222325'),
+        cardTheme: const CardTheme().copyWith(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 5,
+          ),
+        ),
+        textTheme: const TextTheme().copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            fontSize: 14,
+            color: hexStringToColor('#C49653'),
+          ),
+          labelSmall: TextStyle(
+            color: hexStringToColor('#E1D4C8'),
+          ),
+          bodySmall: TextStyle(
+            color: hexStringToColor('#222325'),
+          ),
+          bodyLarge: TextStyle(
+            color: hexStringToColor('#222325'),
+          ),
+        ),
       ),
       home: const Notes(),
     ),
